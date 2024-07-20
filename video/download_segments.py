9d199105d692be7daee4ec9baef8f61b584ea8a0
V-Sarthou <valentin.sarthou@gmail.com>
2024-07-20 10:08:38 +0200

    video download update

--- video/download_segments.py
+++ video/download_segments.py
@@ -11,6 +11,8 @@ import ssl
 url_pattern = "https://vs34.userscontent.net/dash/609473/fragment-{i}-f2-v1-x3.m4s"
 range_vals = [1, 271]
 
+do_download = True
+do_combine = True
 
 def init_dir(dir):
   try:
@@ -20,19 +22,22 @@ def init_dir(dir):
 
 cwd = os.getcwd()
 dir = "segments"
-file_pattern = "seg{i}.m4s"
+file_pattern = "seg{i}.ts"
 file_list = []
+#file_list.append("segments/init-a1-x3.mp4")
 index_range = range(range_vals[0], range_vals[1] + 1)
 for i in index_range:
   file_list.append("{dir}/{file}".format(dir=dir, file=file_pattern.format(i=i)))
 
-context = ssl._create_unverified_context()
-do_download = True
+final_file_list = []
+
 if do_download:
+  context = ssl._create_unverified_context()
   init_dir(os.path.join(cwd, dir))
   download_max_tries = 10
   for local_file, i in zip(file_list, index_range):
     url = str(url_pattern).format(i=i)
+    #url = str(url_pattern).format(i=i, j=(1 + i%4))
     print(url)
     print(local_file)
     sys.stdout.flush()
@@ -40,20 +45,31 @@ if do_download:
     download_num_tries = 0
     while not download_ok:
       try:
-        with urllib.request.urlopen(url, context=context) as response, open(local_file, 'wb') as out_file:
+        h = {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0",
+        }
+        r = urllib.request.Request(url, headers=h)
+        with urllib.request.urlopen(r, context=context) as response, open(local_file, 'wb') as out_file:
           shutil.copyfileobj(response, out_file)
       except TimeoutError:
         print("Could not download file. Retrying...")
+        sys.stdout.flush()
         time.sleep(1)
         download_num_tries += 1
         if download_num_tries > download_max_tries:
           raise
-      else:
+      except Exception as e:
+        print(str(e))
+        sys.stdout.flush()
         download_ok = True
+      else:
+        final_file_list.append(local_file)
+        download_ok = True
+else:
+  final_file_list = file_list
 
-do_combine = True
 if do_combine:
-  combine_cmd = ["ffmpeg", "-i", "concat:{file_list}".format(file_list='|'.join(file_list)), "-c", "copy", "output.mp4"]
+  combine_cmd = ["ffmpeg", "-i", "concat:{file_list}".format(file_list='|'.join(final_file_list)), "-c", "copy", "output.mp4"]
   print(' '.join(combine_cmd))
   sys.stdout.flush()
   subprocess.call(combine_cmd)
