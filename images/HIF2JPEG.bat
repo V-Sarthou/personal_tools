39ec3cf5533ceba7d8979801958bf4dd2fd33d0f
Valentin Sarthou <valentin.sarthou@gmail.com>
2026-09-19 09:57:31 +0200

    images: [dev] Add HIF2JPEG conversion bat scripts

---
+++ images/HIF2JPEG.bat
@@ -0,0 +1,37 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Check if the input is set
+if "%~1"=="" (
+    echo error: file or folder not provided
+    goto :end
+)
+
+:: Check if the input exists
+if not exist "%~1" (
+    echo error: file or folder not found: "%~1"
+    goto :end
+)
+
+:: Check if the input is a directory
+if exist "%~1\" (
+    for %%F in ("%~1\*.HIF") do (
+        call :ConvertFile "%%~fF" "%%~dpnF.jpg"
+    )
+) else (
+    call :ConvertFile "%~1" "%~dpn1.jpg"
+)
+
+:end
+echo Finished
+pause
+goto :eof
+
+
+:: ========================================================
+:: SINGLE CONVERSION SUBROUTINE
+:: ========================================================
+:ConvertFile
+echo Converting %~nx1 to %~nx2
+magick "%~1" -quality 99 "%~2"
+goto :eof
---
+++ images/HIF2JPEG_4K.bat
@@ -0,0 +1,37 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Check if the input is set
+if "%~1"=="" (
+    echo error: file or folder not provided
+    goto :end
+)
+
+:: Check if the input exists
+if not exist "%~1" (
+    echo error: file or folder not found: "%~1"
+    goto :end
+)
+
+:: Check if the input is a directory
+if exist "%~1\" (
+    for %%F in ("%~1\*.HIF") do (
+        call :ConvertFile "%%~fF" "%%~dpnF_4K.jpg"
+    )
+) else (
+    call :ConvertFile "%~1" "%~dpn1_4K.jpg"
+)
+
+:end
+echo Finished
+pause
+goto :eof
+
+
+:: ========================================================
+:: SINGLE CONVERSION SUBROUTINE
+:: ========================================================
+:ConvertFile
+echo Converting %~nx1 to %~nx2
+magick "%~1" -resize 3840x2160 -quality 98 "%~2"
+goto :eof
